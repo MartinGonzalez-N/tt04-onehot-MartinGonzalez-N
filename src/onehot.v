3f7bbc07407721d55d4f5ff1bf975245d073ee51
MartinGonzalez-N <59064436+MartinGonzalez-N@users.noreply.github.com>
2023-09-07 15:37:48 -0600

    Update onehot.v

--- src/onehot.v
+++ src/onehot.v
@@ -1,12 +1,20 @@
-module tt_um_onehot(input[2:0]in, output[7:0]out);
-  
-  assign out[0]= ~in[0] & ~in[1] & ~in[2];
-  assign out[1]= in[0] & ~in[1] & ~in[2];
-  assign out[2]= ~in[0] & in[1] & ~in[2];
-  assign out[3]= in[0] & in[1] & ~in[2];
-  assign out[4]= ~in[0] & ~in[1] & in[2];
-  assign out[5]= in[0] & ~in[1] & in[2];
-  assign out[6]= ~in[0] & in[1] & in[2];
-  assign out[7]= in[0] & in[1] & in[2];
+module tt_um_onehot(
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches for clk_selector and pattern_sel
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 LEDs
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled, not used circuit can be turned off when pattern_sel = 0
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+  assign uo_out[0]= ~ui_in[0] & ~ui_in[1] & ~ui_in[2];
+  assign uo_out[1]= ui_in[0] & ~ui_in[1] & ~ui_in[2];
+  assign uo_out[2]= ~ui_in[0] & ui_in[1] & ~ui_in[2];
+  assign uo_out[3]= ui_in[0] & ui_in[1] & ~ui_in[2];
+  assign uo_out[4]= ~ui_in[0] & ~ui_in[1] & ui_in[2];
+  assign uo_out[5]= ui_in[0] & ~ui_in[1] & ui_in[2];
+  assign uo_out[6]= ~ui_in[0] & ui_in[1] & ui_in[2];
+  assign uo_out[7]= ui_in[0] & ui_in[1] & ui_in[2];
   
 endmodule
